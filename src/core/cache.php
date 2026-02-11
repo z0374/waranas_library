472b5522a27997c5f58edfd40304f214b454b1f7
z0374 <victormacedo2001@hotmail.com>
2026-02-11 15:35:28 -0300

    feat: update cache end render page

--- src/core/cache.php
+++ src/core/cache.php
@@ -1,5 +1,6 @@
 <?php
-function cachePage($title, $content = null, $mode = 'create', $force_update = false) {
+function cachePage($title, $content = null, $mode = 'create', $force_update = false)
+{
     $arquivo = ROOT_PATH_WARANAS_LIB . '/cache/' . md5($title) . '.html';
     $validade = 43200; // 12 horas
 
@@ -7,7 +8,7 @@ function cachePage($title, $content = null, $mode = 'create', $force_update = fa
     if (!$force_update && file_exists($arquivo)) {
         // Se for modo return e expirou, sai para processar
         if ($mode === 'return' && (time() - filemtime($arquivo) > $validade)) {
-            return; 
+            return;
         }
         header("Content-Type: text/html; charset=UTF-8");
         header("X-Cache: HIT");
@@ -15,10 +16,18 @@ function cachePage($title, $content = null, $mode = 'create', $force_update = fa
         exit;
     }
 
-    if ($mode === 'return') return;
+    if ($mode === 'return')
+        return;
 
     // 2. SE FOR CREATE E O CONTEÚDO JÁ VEIO PRONTO (Seu caso na função html)
     if ($mode === 'create' && $content !== null) {
+        $cacheDir = ROOT_PATH_WARANAS_LIB . '/cache';
+
+        // Verifica se a pasta existe, se não, cria com permissões de leitura/escrita
+        if (!is_dir($cacheDir)) {
+            // 0755 é o padrão, true permite criar pastas aninhadas
+            mkdir($cacheDir, 0755, true);
+        }
         file_put_contents($arquivo, $content, LOCK_EX);
         header("Content-Type: text/html; charset=UTF-8");
         header("X-Cache: MISS-STORED");
