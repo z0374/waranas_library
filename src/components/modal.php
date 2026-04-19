5d9166923d8fa1085d58e572e9f02856d7eb8568
Z0374 <git@Z0374.com>
2026-04-19 19:30:08 -0300

    fix: new modal id rule

--- src/components/modal.php
+++ src/components/modal.php
@@ -16,18 +16,22 @@ function modal($id, $array, $color) {
 
     // Estilos dinâmicos
     $style[] = "
-        #modal-toggle{$id}:checked + #{$id}Mdl{visibility:visible;opacity:1;}
-        #{$id} .cmdl{border-top: solid 6vh {$color};}
-        #{$id}{$bt}{background-color:#EAC7EB;padding:3% 15%;border-radius:0.9vh;background-color:{$color};}
+        #modal-toggle{$distinctId}:checked + #{$distinctId}Mdl{visibility:visible;opacity:1;}
+        #{$distinctId} .cmdl{border-top: solid 6vh {$color};}
+        #{$distinctId}{$bt}{background-color:#EAC7EB;padding:3% 15%;border-radius:0.9vh;background-color:{$color};}
     ";
     $mobile[] = ".cmdl{width:90%;height:69%;}";
 
     $linha = '';
+
+    $lowerId = mb_strtolower($id, 'UTF-8');
+    $distinctId = $lowerId . distinctDigits();
+
     if (is_array($array)) {
         for ($v = 0; $v < $length - 1; $v++) {
-            $item = "<a id='{$id}{$v}' href={$array[$v]['href']}>{$array[$v]['content']}</a>";
+            $item = "<a id='{$distinctId}{$v}' href={$array[$v]['href']}>{$array[$v]['content']}</a>";
             if (array_key_exists('lnk', $array[$v])) {
-                $item .= "<a id='{$id}i{$v}' href='{$array[$v]['lnk']}' target='blank'><i id='i'class='fa-solid fa-arrow-up-right-from-square' aria-hidden='true'></i></a>";
+                $item .= "<a id='{$distinctId}i{$v}' href='{$array[$v]['lnk']}' target='blank'><i id='i'class='fa-solid fa-arrow-up-right-from-square' aria-hidden='true'></i></a>";
             }
             $linha .= "<span id='itm'>{$item}</span>";
         }
@@ -37,12 +41,13 @@ function modal($id, $array, $color) {
 
     $imdl = "<div class='content'>{$linha}</div>";
     if (is_array($array)) {
-        $imdl .= "<a class='mbt' href='/' id='{$id}{$bt}'>{$array[($length-1)]['content']}</a>";
+        $imdl .= "<a class='mbt' href='/' id='{$distinctId}{$bt}'>{$array[($length-1)]['content']}</a>";
     } else {
-        $imdl .= "<label class='closeX' for='modal-toggle{$id}'>&times;</label>";
+        $imdl .= "<label class='closeX' for='modal-toggle{$distinctId}'>&times;</label>";
     }
 
-    $mdl[] = "<label class='closeModal' for='modal-toggle{$id}'></label><div class='cmdl' onclick='event.stopPropagation()'>{$imdl}</div>";
-    $body[] = "<input type='checkbox' id='modal-toggle{$id}' class='modal-toggle' /><div id='{$id}Mdl' class='mdl'>" . implode('', $mdl) . "</div>";
-    return '<label id="' . $id . '" for="modal-toggle' . $id . '" class="openModal">' . $id . '</label>';
+    $mdl[] = "<label class='closeModal' for='modal-toggle{$distinctId}'></label><div class='cmdl' onclick='event.stopPropagation()'>{$imdl}</div>";
+    $body[] = "<input type='checkbox' id='modal-toggle{$distinctId}' class='modal-toggle' /><div id='{$distinctId}Mdl' class='mdl {$lowerId}'>" . implode('', $mdl) . "</div>";
+
+    return '<label id="' . $lowerId . '" for="modal-toggle' . $distinctId . '" class="openModal">' . $id . '</label>';
 }
---
+++ src/utils/distinctDigits.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Gera uma sequência de 6 dígitos únicos e aleatórios.
+ * Foca em performance usando referências de array e shuffle.
+ * * @return string
+ */
+function distinctDigits(): string {
+    $digitos = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    
+    // Embaralha o array de dígitos (altamente otimizado no core do PHP)
+    shuffle($digitos);
+    
+    // Retorna os 6 primeiros elementos unidos
+    return $digitos[0] . $digitos[1] . $digitos[2] . $digitos[3] . $digitos[4] . $digitos[5];
+}
+
+// Exemplo de uso:
+echo gerarCodigoUnicoRapido();
