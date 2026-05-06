3ccb1c2bbffe5cf121e94b9ea875fc82ce683ce6
Z0374 <git@Z0374.com>
2026-05-06 13:53:22 -0300

    fix: BRL furmatter function

--- src/helpers/formatters.php
+++ src/helpers/formatters.php
@@ -1,9 +1,28 @@
 <?php
+/**
+ * Transforma um valor numérico ou string em formato de moeda BRL (Real).
+ * Garante que 44 se torne R$ 44,00 e 44.99 se torne R$ 44,99.
+ * @param mixed $valor
+ * @return string
+ */
 function BRL($valor) {
-    $valor = floatval(preg_replace('/[^\d,.]/', '', $valor));
-    return 'R$ ' . number_format($valor, 2, ',', '.');
+    // 1. Remove qualquer símbolo de moeda ou espaço
+    // 2. Substitui a vírgula por ponto para que o floatval reconheça os decimais
+    $valorLimpo = str_replace(',', '.', preg_replace('/[^\d,.]/', '', $valor));
+    
+    // 3. Converte para float (ex: "44.99" vira 44.99)
+    $valorFloat = floatval($valorLimpo);
+    
+    // 4. Formata com 2 casas decimais, vírgula para decimal e ponto para milhar
+    return 'R$ ' . number_format($valorFloat, 2, ',', '.');
 }
 
+/**
+ * Formata um CEP para o padrão 00000-000.
+ * @param string $cep
+ * @return string
+ * @throws Exception
+ */
 function cepFormat($cep) {
     // Remove todos os caracteres não numéricos
     $numericCEP = preg_replace('/\D/', '', $cep);
